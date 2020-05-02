3d716331a3f57a0c677a67864443b236f1256086
moxxiq <dimanavsisto@gmail.com>
2020-05-02 19:19:18 +0300

    Changed Credit max length

--- lab 2/create.sql
+++ lab 2/create.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06d332d73a28c005f5f4ab7fd8ab8b39b5876e89321e9fa88de0d75aeb98d4ba
+oid sha256:5ee2e5faa97a2cf8c3e4e343cc515c5df4df17e4437fad8055b41355192f1197
 size 1898
