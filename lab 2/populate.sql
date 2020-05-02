596914d0a8256700749ec91978ed21a4cbf73d50
moxxiq <dimanavsisto@gmail.com>
2020-05-02 17:30:48 +0300

    Working solution closes #1

---
+++ lab 2/populate.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c46ef183139f590b7e4b5afe883e6bfd9123c1e82025f460ce5a6128463721df
+size 205261663
--- lab 2/query.sql
+++ lab 2/query.sql
@@ -1,26 +1,3 @@
--- Запит №1: Вивести 10 авторів з найбільшою кількістю робіт та кількість робіт кожного з цих авторів.
--- Візуалізація: стовпчикова діаграма.
-SELECT *
-FROM (SELECT ARTIST_ID, ARTIST_NAME, COUNT(RAA.ARTWORK_ARTWORK_ID) ARTWORKS_COUNT
-      FROM ARTIST
-               LEFT JOIN RELATION_ARTWORK_ARTIST RAA on ARTIST.ARTIST_ID = RAA.ARTIST_ARTIST_ID
-      GROUP BY ARTIST_ID, ARTIST_NAME
-      ORDER BY ARTWORKS_COUNT DESC)
-WHERE ROWNUM <= 10;
--- Oracle 11g does not support 'FETCH FIRST 10 ROWS ONLY' syntax
-
--- Запит №2: Вивести постачальників робіт музею у відсотковому співвідношенні відповідно до кількості робіт, які вони передали музею.
--- Візуалізація: секторна діаграма
-SELECT PROC_OFFICER.PROC_OFFICER_NAME, COUNT(ARTWORK_ID) ARTWORKS_CREDITED_COUNT
-FROM PROC_OFFICER
-         JOIN ARTWORK on PROC_OFFICER.PROC_OFFICER_NAME = ARTWORK.PROC_OFFICER_NAME
-GROUP BY PROC_OFFICER.PROC_OFFICER_NAME
-ORDER BY ARTWORKS_CREDITED_COUNT DESC;
-
-
--- Запит №3: Вивести динаміку кількості робіт по роках.
--- Візуалізація: графік залежності
-SELECT ARTWORK_CREATION_YEAR, COUNT(ARTWORK_ID) NUMBER_OF_ARTWORKS
-FROM ARTWORK
-GROUP BY ARTWORK_CREATION_YEAR
-ORDER BY ARTWORK_CREATION_YEAR;
+version https://git-lfs.github.com/spec/v1
+oid sha256:576e2c924f1eab88a89b6811afa4e79fc63eff9143fe48379c6feb7a0c287281
+size 1494
